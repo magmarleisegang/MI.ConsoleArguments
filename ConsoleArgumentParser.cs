3c04d660d3bfb02a52256196269d6b9a80399380
magdalena.leisegang <magdalena.leisegang@entelect.co.za>
2019-10-06 20:51:01 +0200

    initial code commit

---
+++ ConsoleArgumentParser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MI.ConsoleArguments
+{
+    public class ConsoleArgumentParser
+    {
+        public static T ParseArgs<T>(string[] args, Dictionary<string, Action<string, T>> argsList)
+            where T : new()
+        {
+           T details = new T();
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (argsList.ContainsKey(args[i]))
+                {
+                    argsList[args[i]].Invoke(args[++i], details);
+                }
+            }
+            return details;
+        }        
+    }
+}
---
+++ ConsoleArgumentPrompter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MI.ConsoleArguments
+{
+    public static class ConsoleArgumentPrompter
+    {
+        public static string PromptUser(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+    }
+}
